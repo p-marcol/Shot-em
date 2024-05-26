5f36d0fd8f14554a324a81d14197c96a0ce88530
Jakub-Barylak <kubas2001@poczta.fm>
2024-05-26 16:37:04 +0200

    Added SaveAreaView

--- app/app/event/[eventId].tsx
+++ app/app/event/[eventId].tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { fetchEvent } from "@lib/fireStoreHelpers";
+import TopBar from "components/topbar";
 export default function EventPage() {
 	const { eventId } = useLocalSearchParams();
 	const [eventDetails, setEventDetails] = useState();
@@ -13,9 +15,12 @@ export default function EventPage() {
 		fetchEventDetails();
 	}, []);
 	return (
-		<View>
-			<Text>Tutaj będą zdjęcia :)</Text>
-			<Text>{JSON.stringify(eventDetails)}</Text>
-		</View>
+		<SafeAreaView style={{ backgroundColor: "black" }}>
+			<View className="bg-[#F8F1E8] w-full h-full">
+				<TopBar showBackButton={true} showShadow={true} />
+				<Text>Tutaj będą zdjęcia :)</Text>
+				<Text>{JSON.stringify(eventDetails)}</Text>
+			</View>
+		</SafeAreaView>
 	);
 }
